8da09954616a8c56b1f6be94cb5802b42da39466
DSGS76 <duvansalinas1907@gmail.com>
2024-10-12 17:48:37 -0500

    Update of the branch database with some adjusts and comments in properties. And the auto creation of the container of the database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 drop table if exists pokemon;
-create table pokemon(
+create table if not exists pokemon(
     id Integer not null primary key,
     name varchar(32) not null,
     height Integer not null,
