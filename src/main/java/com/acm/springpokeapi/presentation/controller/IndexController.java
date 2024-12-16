78c9775037084086ff2563d38e7d92fb06122a83
DSGS76 <duvansalinas1907@gmail.com>
2024-12-16 18:23:00 -0500

    Some minor improvements

--- src/main/java/com/acm/springpokeapi/presentation/controller/IndexController.java
+++ src/main/java/com/acm/springpokeapi/presentation/controller/IndexController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -35,7 +32,7 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/envioinfo")
+    @PostMapping("/envioinfo")
     public String enviarInfo(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
         logger.info(name);
